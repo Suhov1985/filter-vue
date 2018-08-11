b3dc8485197f80c112b413ccc4a00fc06d59db86
Suhov1985 <Suhov1985@gmail.com>
2018-08-11 13:51:09 +0300

    filter-vue

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.selectpicker').selectpicker({
+		size: 4
+	});
+	$('.filter-selectpicker').selectpicker({
+		size: 2
+	});
+	$(".navbar-toggle").click(function (  ) {
+		$(this).toggleClass('active');
+	});
+});
+
+
+
+
+
+
